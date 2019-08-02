5e9c7c04ef19eb4dde5aa82a7712ac5e4be230b4
Dallen Hoyal <dallen.hoyal@gmail.com>
2019-08-02 13:57:05 -0600

    updated classCtrl

--- controllers/classCtrl.js
+++ controllers/classCtrl.js
@@ -1,30 +1,45 @@
-const classData = require('../json/classes.json');
+const db = require('../firebase');
 
 module.exports = {
 
     getAllClasses: (req, res) => {
-        let data = classData;
-        if (req.query.name) {
-            data = data.filter(item => {
-                return (
-                    item.name.toUpperCase().includes(req.query.name.toUpperCase())
-                );
+        let data = [];
+
+        db.collection('classes').get()
+            .then((query) => {
+                query.forEach((class) => {
+                    data.push(class.data());
+                });
+
+                if (req.query.name) {
+                    data = data.filter(item => {
+                        return (
+                            item.name.toUpperCase().includes(req.query.name.toUpperCase())
+                        );
+                    });
+                }
+
+                res.status(200).send(data);
             });
-        }
-        res.status(200).send(data);
     },
 
     getClassById: (req, res) => {
-        let data = classData.filter(item => {
-            return (
-                item.id == req.params.id
-            );
-        });
-        if (data[0]) {
-            res.status(200).send(data[0]);
-        } else {
-            res.status(404).send({});
-        }
+        let data = [];
+
+        db.collection('classes').get()
+            .then((query) => {
+                query.forEach((class) => {
+                    if (req.params.id == class.data().id) {
+                        data.push(class.data());
+                    }
+                });
+
+                if (data[0]) {
+                    res.status(200).send(data[0]);
+                } else {
+                    res.status(404).send({});
+                }
+            });
     }
 
 }
