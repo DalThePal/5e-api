ccf348cbc484e70b1713de1f94ed720e8f9dfb47
Dallen Hoyal <dallen.hoyal@gmail.com>
2019-08-04 13:14:37 -0600

    updated subclassCtrl

--- controllers/subclassCtrl.js
+++ controllers/subclassCtrl.js
@@ -1,43 +1,54 @@
-const subclassData = require('../json/subclasses.json');
+const db = require('../firebase');
+
 
 module.exports = {
 
     getAllSubclasses: (req, res) => {
-        let data = subclassData;
-        if (req.query.name) {
-            data = data.filter(subclass => {
-                return (
-                    subclass.name.toUpperCase().includes(req.query.name.toUpperCase())
-                );
+        let data = [];
+
+        db.collection('subclasses').get()
+            .then(query => {
+                query.forEach(subclass => {
+                    data.push(subclass.data());
+                });
+
+                if (req.query.name) {
+                    data = data.filter(subclass => {
+                        return (
+                            subclass.name.toUpperCase().includes(req.query.name.toUpperCase())
+                        );
+                    });
+                }
+                if (req.query.flavor) {
+                    data = data.filter(subclass => {
+                        return (
+                            subclass.subclass_flavor.toUpperCase().includes(req.query.flavor.toUpperCase())
+                        );
+                    });
+                }
+                if (req.query.class) {
+                    data = data.filter(subclass => {
+                        return (
+                            subclass.class.name.toUpperCase().includes(req.query.class.toUpperCase())
+                        );
+                    });
+                }
+
+                res.status(200).send(data);
             });
-        }
-        if (req.query.flavor) {
-            data = data.filter(subclass => {
-                return (
-                    subclass.subclass_flavor.toUpperCase().includes(req.query.flavor.toUpperCase())
-                );
-            });
-        }
-        if (req.query.class) {
-            data = data.filter(subclass => {
-                return (
-                    subclass.class.name.toUpperCase().includes(req.query.class.toUpperCase())
-                );
-            });
-        }
-        res.status(200).send(data);
     },
 
     getSubclassById: (req, res) => {
-        let data = subclassData.filter(item => {
-            return (
-                item.id == req.params.id
-            );
-        });
-        if (data[0]) {
-            res.status(200).send(data[0]);
-        } else {
-            res.status(404).send({});
-        }
+        let subclass = db.collection('subclasses').doc(req.params.id);
+
+        subclass.get()
+            .then(doc => {
+
+                if (doc.exists) {
+                    res.status(200).send(doc.data());
+                } else {
+                    res.status(404).send({});
+                }
+            });
     }
 }
