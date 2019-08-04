6f88b83ddf2e92f9310f301f2f327b8e199346ab
Dallen Hoyal <dallen.hoyal@gmail.com>
2019-08-04 13:10:00 -0600

    updated skillCtrl

--- controllers/skillCtrl.js
+++ controllers/skillCtrl.js
@@ -1,28 +1,39 @@
-const skillData = require('../json/skills.json');
+const db = require('../firebase');
 
 module.exports = {
 
     getAllSkills: (req, res) => {
-        let data = skillData;
-        if (req.query.name) {
-            data = data.filter(skill => {
-                return (
-                    skill.name.toUpperCase().includes(req.query.name.toUpperCase())
-                );
+        let data = [];
+
+        db.collection('skills').get()
+            .then(query => {
+                query.forEach(skill => {
+                    data.push(skill.data());
+                });
+
+                if (req.query.name) {
+                    data = data.filter(skill => {
+                        return (
+                            skill.name.toUpperCase().includes(req.query.name.toUpperCase())
+                        );
+                    });
+                }
+
+                res.status(200).send(data);
             });
-        }
-        res.status(200).send(data);
     },
 
     getSkillById: (req, res) => {
-        let data = skillData.filter(item => {
-            return (
-                item.id == req.params.id
-            );
-        });
-        if (data[0]) {
-            res.status(200).send(data[0]);
-        } else {
-            res.status(404).send({});
-        }    }
+        let skill = db.collection('skills').doc(req.params.id);
+
+        skill.get()
+            .then(doc => {
+
+                if (doc.exists) {
+                    res.status(200).send(doc.data());
+                } else {
+                    res.status(404).send({});
+                }    
+            });
+    }
 }
