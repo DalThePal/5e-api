451bbf47cf947739e50cdd611977d702ae2ea7cb
Dallen Hoyal <dallen.hoyal@gmail.com>
2019-08-04 13:04:06 -0600

    fixed classCtrl and updated proficiencyCtrl

--- controllers/classCtrl.js
+++ controllers/classCtrl.js
@@ -7,8 +7,8 @@ module.exports = {
 
         db.collection('classes').get()
             .then((query) => {
-                query.forEach((class) => {
-                    data.push(class.data());
+                query.forEach((item) => {
+                    data.push(item.data());
                 });
 
                 if (req.query.name) {
@@ -24,22 +24,16 @@ module.exports = {
     },
 
     getClassById: (req, res) => {
-        let data = [];
+        let data = db.collection('classes').doc(req.params.id);
 
-        db.collection('classes').get()
-            .then((query) => {
-                query.forEach((class) => {
-                    if (req.params.id == class.data().id) {
-                        data.push(class.data());
-                    }
-                });
+        data.get()
+            .then(doc => {
 
-                if (data[0]) {
-                    res.status(200).send(data[0]);
+                if (doc.exists) {
+                    res.status(200).send(doc.data());
                 } else {
                     res.status(404).send({});
                 }
-            });
+            });  
     }
-
 }
